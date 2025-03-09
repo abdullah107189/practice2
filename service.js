a8dd48f8808c20c874c4df552509a2ac4305eebe
siam114 <smsiamislam598@gmail.com>
2025-03-09 21:26:10 +0600

    join str

--- service.js
+++ service.js
@@ -2,6 +2,18 @@
 const siam = 123;
 let s = "siam";
 var k= "islam"
-console.table([siam,s,k]);
-console.log(typeof s)
-console.log(typeof siam)
+// console.table([siam,s,k]);
+// console.log(typeof s)
+// console.log(typeof siam)
+let val= 3
+// let negval=-val
+// console.log(negval)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+let str1 = "SM Siam"
+let str2 = " islam"
+let str3 = str1 + str2
+console.log(str3)
