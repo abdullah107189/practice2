fc9ae5dd8fac1561a6eb266a176bc6a89e3c2949
siam114 <smsiamislam598@gmail.com>
2025-03-10 14:00:49 +0600

    js practice

--- service.js
+++ service.js
@@ -12,8 +12,12 @@ let val= 3
 // console.log(2*2)
 // console.log(2**2)
 // console.log(2/2)
-console.log(2%2)
+// console.log(2%2)
 let str1 = "SM Siam"
 let str2 = " islam"
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
+
+// console.log("2" === 1)
+// console.log("02" > 1)
+
